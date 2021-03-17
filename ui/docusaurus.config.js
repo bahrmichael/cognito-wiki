0efc90265508c5f1cb04e788ff461456e8a9882f
Michael Bahr <michael@bahr.dev>
2021-03-17 11:06:12 +0100

    reduced sidebar count because the scrollbar looks ugly

--- ui/docusaurus.config.js
+++ ui/docusaurus.config.js
@@ -90,7 +90,6 @@ module.exports = {
           blogDescription: 'Latest content about Amazon Cognito',
           showReadingTime: false,
           postsPerPage: 10,
-          blogSidebarCount: 10,
           editUrl:
             'https://github.com/bahrmichael/cognito-wiki/edit/main/ui/feed/',
           feedOptions: {
